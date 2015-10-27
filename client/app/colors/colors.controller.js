af8a241421b2cd08af1b0eebbf3da874f5f88520
Steven Lytovka <ss_imperator@inbox.ru>
2015-10-26 21:09:37 -0400

    Added submit button

--- client/app/colors/colors.controller.js
+++ client/app/colors/colors.controller.js
@@ -3,24 +3,49 @@
 angular.module('zenColorApp')
   .controller('ColorsCtrl', function ($scope) {
     $scope.message = 'Hello';
+    $scope.isSubmit = false;
+    $scope.color1 = false;
+    $scope.color2 = false;
+    $scope.color3 = false;
     $scope.colorize = function(color) {
-      if (color == 'blue')
+      if (color == 'blue') {
       $scope.myStyle1={'background-color': 'blue'}
-      else if(color =='red')
+      $scope.color1 = true;
+       }
+      else if(color =='red') {
       $scope.myStyle1={'background-color': 'red'}
-      else if(color =='yellow')
+      $scope.color1 = true;
+       }
+      else if(color =='yellow') {
       $scope.myStyle1={'background-color': 'yellow'}
-      else if(color =='green')
+      $scope.color1 = true;
+       }
+      else if(color =='green') {
       $scope.myStyle2={'background-color': 'green'}
-      else if(color =='purple')
+      $scope.color2 = true;
+       }
+      else if(color =='purple') {
       $scope.myStyle2={'background-color': 'purple'}
-      else if(color =='orange')
+      $scope.color2 = true;
+       }
+      else if(color =='orange') {
       $scope.myStyle2={'background-color': 'orange'}
-      else if(color =='black')
+      $scope.color2 = true;
+       }
+      else if(color =='black') {
       $scope.myStyle3={'background-color': 'black'}
-      else if(color =='white')
+      $scope.color3 = true;
+       }
+      else if(color =='white') {
       $scope.myStyle3={'background-color': 'white'}
-      else if(color =='brown')
+      $scope.color3 = true;
+       }
+      else if(color =='brown') {
       $scope.myStyle3={'background-color': 'brown'}
+      $scope.color1 = true;
+        }
     }
+
+
+
   });
