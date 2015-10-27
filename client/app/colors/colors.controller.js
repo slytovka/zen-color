4bb1b3a51804389384d4b94b0c94663225ce679f
Steven Lytovka <ss_imperator@inbox.ru>
2015-10-26 22:26:11 -0400

    Added contact form, changed buttons

--- client/app/colors/colors.controller.js
+++ client/app/colors/colors.controller.js
@@ -10,40 +10,44 @@ angular.module('zenColorApp')
     $scope.colorize = function(color) {
       if (color == 'blue') {
       $scope.myStyle1={'background-color': 'blue'}
-      $scope.color1 = true;
+      $scope.color1 = 'blue';
        }
       else if(color =='red') {
       $scope.myStyle1={'background-color': 'red'}
-      $scope.color1 = true;
+      $scope.color1 = 'red';
        }
       else if(color =='yellow') {
       $scope.myStyle1={'background-color': 'yellow'}
-      $scope.color1 = true;
+      $scope.color1 = 'yellow';
        }
       else if(color =='green') {
       $scope.myStyle2={'background-color': 'green'}
-      $scope.color2 = true;
+      $scope.color2 = 'green';
        }
       else if(color =='purple') {
       $scope.myStyle2={'background-color': 'purple'}
-      $scope.color2 = true;
+      $scope.color2 = 'purple';
        }
       else if(color =='orange') {
       $scope.myStyle2={'background-color': 'orange'}
-      $scope.color2 = true;
+      $scope.color2 = 'orange';
        }
       else if(color =='black') {
       $scope.myStyle3={'background-color': 'black'}
-      $scope.color3 = true;
+      $scope.color3 = 'black';
        }
       else if(color =='white') {
       $scope.myStyle3={'background-color': 'white'}
-      $scope.color3 = true;
+      $scope.color3 = 'white';
        }
       else if(color =='brown') {
       $scope.myStyle3={'background-color': 'brown'}
-      $scope.color1 = true;
+      $scope.color3 = 'brown';
         }
+    }
+
+    $scope.getResults = function() {
+
     }
 
 
